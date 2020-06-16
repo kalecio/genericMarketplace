77a276183e706de12c6cb36783ccf33b11fe48c1
Kalécio Pereira <kalecio.santana@gmail.com>
2020-06-15 21:00:18 -0300

    criado controller resource de produtos

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    private $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = $this->product->paginate(10);
+
+        return view('admin.products.index', compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $stores = \App\Store::all(['id', 'name']);
+        
+        return view('admin.products.create', compact('stores'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        // return $data;
+
+        $store =  \App\Store::find($data['store']);
+        $store->products()->create($data);
+
+        flash('Produto criado com sucesso!')->success();
+        return redirect()->route('admin.products.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show($product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($product)
+    {
+        $product = $this->product->find($product);
+
+        return view('admin.products.edit', compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $product)
+    {
+        $data = $request->all();
+
+        $product = $this->product->find($product);
+        $product->update($data);
+
+        flash('Produto atualizado com sucesso!')->success();
+        return redirect()->route('admin.products.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($product)
+    {
+       
+        $product = $this->product->find($product);
+        $product->delete();
+
+        flash('Produto removido com sucesso!')->success();
+        return redirect()->route('admin.products.index');
+    }
+}
