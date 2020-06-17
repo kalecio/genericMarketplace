e67a15dd54aa58a747e819286428932971b15228
Kalécio Pereira <kalecio.santana@gmail.com>
2020-06-17 00:10:46 -0300

    Botão de logout criado

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/model', function() {
 
@@ -54,4 +54,4 @@ Route::group(['middleware' => ['auth']], function(){
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
