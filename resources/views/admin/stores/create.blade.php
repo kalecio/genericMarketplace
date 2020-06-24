beb768428467199308964b0042af39d840568fae
Kalécio Pereira <kalecio.santana@gmail.com>
2020-06-23 22:02:36 -0300

    Adicionado validações em formulário de criação de lojas

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -4,25 +4,45 @@
     <h1>Criar Loja</h1>
     <!-- passando o action do form de forma dinâmica pegando o apelido da rota -->
     <form action="{{route('admin.stores.store')}}" method="post">
-        <input type="hidden" name="_token" value="{{csrf_token()}}">
+        @csrf
         <div class="form-group">
             <label for="">Nome Loja</label>
-            <input type="text" name="name" class="form-control">
+            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror">
+            @error('name')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="">Descrição</label>
-            <input type="text" name="description" class="form-control">
+            <input type="text" name="description" class="form-control @error('description') is-invalid @enderror">
+            @error('description')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="">Telefone</label>
-            <input type="text" name="phone" class="form-control">
+            <input type="text" name="phone" class="form-control @error('phone') is-invalid @enderror">
+            @error('phone')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="">Celular/Whatsapp</label>
-            <input type="text" name="mobile_phone" class="form-control">
+            <input type="text" name="mobile_phone" class="form-control @error('mobile_phone') is-invalid @enderror">
+            @error('mobile_phone')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="form-group">
