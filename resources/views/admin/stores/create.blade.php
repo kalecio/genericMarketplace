56352f56de2ae3440570668f641ad6d216d405bb
Kalécio Pereira <kalecio.santana@gmail.com>
2020-06-23 22:11:19 -0300

    Mantendo estado de formulário de criação de lojas após submit

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -7,7 +7,7 @@
         @csrf
         <div class="form-group">
             <label for="">Nome Loja</label>
-            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror">
+        <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{old('name')}}">
             @error('name')
             <div class="invalid-feedback">
                 {{$message}}
@@ -17,7 +17,7 @@
 
         <div class="form-group">
             <label for="">Descrição</label>
-            <input type="text" name="description" class="form-control @error('description') is-invalid @enderror">
+            <input type="text" name="description" class="form-control @error('description') is-invalid @enderror" value="{{old('description')}}">
             @error('description')
             <div class="invalid-feedback">
                 {{$message}}
@@ -27,7 +27,7 @@
 
         <div class="form-group">
             <label for="">Telefone</label>
-            <input type="text" name="phone" class="form-control @error('phone') is-invalid @enderror">
+            <input type="text" name="phone" class="form-control @error('phone') is-invalid @enderror" value="{{old('phone')}}">
             @error('phone')
             <div class="invalid-feedback">
                 {{$message}}
@@ -37,7 +37,7 @@
 
         <div class="form-group">
             <label for="">Celular/Whatsapp</label>
-            <input type="text" name="mobile_phone" class="form-control @error('mobile_phone') is-invalid @enderror">
+            <input type="text" name="mobile_phone" class="form-control @error('mobile_phone') is-invalid @enderror" value="{{old('mobile_phone')}}">
             @error('mobile_phone')
             <div class="invalid-feedback">
                 {{$message}}
@@ -47,7 +47,7 @@
 
         <div class="form-group">
             <label for="">Slug</label>
-            <input type="text" name="slug" class="form-control">
+            <input type="text" name="slug" class="form-control" value="{{old('slug')}}">
         </div>
 
         <div>
