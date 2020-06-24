1dfe2aa799ec686125573e75bb5163d57341b7bc
Kalécio Pereira <kalecio.santana@gmail.com>
2020-06-23 23:35:27 -0300

    Adicionado validações as views de criação e edição de products

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -1,42 +1,59 @@
 @extends('layouts.app')
-<!-- informando o nome do layout e englobando o nosso template dentro da section -->
+
+
 @section('content')
     <h1>Criar Produto</h1>
-    <!-- passando o action do form de forma dinâmica pegando o apelido da rota -->
     <form action="{{route('admin.products.store')}}" method="post">
         @csrf
+
         <div class="form-group">
-            <label for="">Nome Produto</label>
-            <input type="text" name="name" class="form-control">
+            <label>Nome Produto</label>
+            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{old('name')}}">
+
+            @error('name')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="form-group">
-            <label for="">Descrição</label>
-            <input type="text" name="description" class="form-control">
+            <label>Descrição</label>
+            <input type="text" name="description" class="form-control @error('description') is-invalid @enderror" value="{{old('description')}}">
+
+            @error('description')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="form-group">
-            <label for="">Conteúdo</label>
-            <textarea name="body" id="" cols="30" rows="10" class="form-control"></textarea>
+            <label>Conteúdo</label>
+            <textarea name="body" id="" cols="30" rows="10" class="form-control @error('body') is-invalid @enderror">{{old('body')}}</textarea>
+
+            @error('body')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
+        </div>
+
+
+        <div class="form-group">
+            <label>Preço</label>
+            <input type="text" name="price" class="form-control @error('price') is-invalid @enderror" value="{{old('price')}}">
+
+            @error('price')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="form-group">
-            <label for="">Preço</label>
-            <input type="text" name="price" class="form-control">
-        </div>
-
-        <div class="form-group">
-            <label for="">Slug</label>
+            <label>Slug</label>
             <input type="text" name="slug" class="form-control">
-        </div>
-
-        <div class="form-group">
-            <label for="">Lojas</label>
-            <select name="store" class="form-control">
-                @foreach($stores as $store)
-                    <option value="{{$store->id}}">{{$store->name}}</option>
-                @endforeach
-            </select>
         </div>
 
         <div>
--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -1,34 +1,60 @@
 @extends('layouts.app')
-<!-- informando o nome do layout e englobando o nosso template dentro da section -->
+
+
 @section('content')
     <h1>Atualizar Produto</h1>
-    <!-- passando o action do form de forma dinâmica pegando o apelido da rota -->
+
     <form action="{{route('admin.products.update', ['product' => $product->id])}}" method="post">
         @csrf
         @method("PUT")
 
         <div class="form-group">
-            <label for="">Nome Produto</label>
-            <input type="text" name="name" class="form-control" value="{{$product->name}}">
+            <label>Nome Produto</label>
+            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{$product->name}}">
+
+            @error('name')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="form-group">
-            <label for="">Descrição</label>
-            <input type="text" name="description" class="form-control" value="{{$product->description}}">
+            <label>Descrição</label>
+            <input type="text" name="description" class="form-control @error('description') is-invalid @enderror" value="{{$product->description}}">
+
+            @error('description')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="form-group">
-            <label for="">Conteúdo</label>
-            <textarea name="body" id="" cols="30" rows="10" class="form-control">{{$product->body}}</textarea>
+            <label>Conteúdo</label>
+            <textarea name="body" id="" cols="30" rows="10" class="form-control @error('body') is-invalid @enderror">{{$product->body}}</textarea>
+
+            @error('body')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
+        </div>
+
+
+        <div class="form-group">
+            <label>Preço</label>
+            <input type="text" name="price" class="form-control @error('price') is-invalid @enderror" value="{{$product->price}}">
+
+            @error('price')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="form-group">
-            <label for="">Preço</label>
-            <input type="text" name="price" class="form-control" value="{{$product->price}}">
-        </div>
-
-        <div class="form-group">
-            <label for="">Slug</label>
+            <label>Slug</label>
             <input type="text" name="slug" class="form-control" value="{{$product->slug}}">
         </div>
 
