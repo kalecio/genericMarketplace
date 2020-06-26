e855ef8bad12c23c3ca937a6a31446eaa688384d
Kalécio Pereira <kalecio.santana@gmail.com>
2020-06-25 22:02:49 -0300

    Criado Rota de categories

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,7 @@ Route::group(['middleware' => ['auth']], function(){
     
         Route::resource('stores', 'StoreController');
         Route::resource('products', 'ProductController');
+        Route::resource('categories', 'CategoryController');
     
     });
 });
