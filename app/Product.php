cd60f58f3eca76c03914e44ced532c56af5887ff
Kalécio Pereira <kalecio.santana@gmail.com>
2020-07-09 02:13:44 -0300

    feat(model): adicionada relação entre Product e ProductPhoto

--- app/Product.php
+++ app/Product.php
@@ -17,4 +17,9 @@ class Product extends Model
     {
         return $this->belongsToMany(Category::class);
     }
+
+    public function photos()
+    {
+        return $this->hasMany(ProductPhoto::class);
+    }
 }
